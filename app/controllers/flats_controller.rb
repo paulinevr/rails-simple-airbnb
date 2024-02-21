5351390750897b838385129cee6d0717aba6db1e
Pauline van Rijckevorsel <vr.pauline@gmail.com>
2024-02-21 18:01:11 +0100

    rails

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,41 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = flat.new
+  end
+
+  def create
+    @flat = flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:title, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+Flat.create!(
+  name: "BABEL",
+  address: "Berlin",
+  description: "Light & Spacious Garden Flat",
+  price_per_night: 110,
+  number_of_guests: 3
+  )
+
+Flat.create!(
+  name: "TERMINUS",
+  address: "London",
+  description: "Tiny hotel with 18 unique rooms",
+  price_per_night: 178,
+  number_of_guests: 8
+  )
+
+Flat.create!(
+  name: "MOB",
+  address: "Madrid",
+  description: "Art Deco jewel, wildly chic and elegant",
+  price_per_night: 80,
+  number_of_guests: 6
+  )
